36c64ccc8a2e15c947513edf8964b4692a600bb6
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-06 00:25:12 +0900

    3章【一覧画面を表示しよう】

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "lists/new"
-  get "lists/index"
-  get "lists/show"
-  get "lists/edit"
   get "/top" => "homes#top"
   post "lists" => "lists#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "lists" => "lists#index"
 end
