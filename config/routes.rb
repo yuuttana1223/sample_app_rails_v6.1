377a9d6d401ac24e3a74485eede20f453cd82221
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-06 01:03:25 +0900

    5章【編集機能を作ろう】

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post "lists" => "lists#create"
   get "lists" => "lists#index"
   get "lists/:id" => "lists#show", as: "list"
+  get "lists/:id/edit" => "lists#edit", as: "edit_list"
+  patch "lists/:id" => "lists#update", as: "update_list"
 end
