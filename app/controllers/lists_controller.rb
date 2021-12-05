0cdaa706d81752bebae7b388f436b304e6f6758e
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-05 22:07:20 +0900

    2章【投稿機能を作ろう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to "/top"
   end
 
   def index
@@ -9,5 +16,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
